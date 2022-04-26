5a7676a9fa308d7c9bad6ad646ed08bd8b2c6f90
anandhan07 <anandhasayanan007jb@gmail.com>
2022-04-26 20:14:39 +0530

    products: Introduce Redmi 5 Plus (vince)
    Signed-off-by: anandhan07 <anandhasayanan007jb@gmail.com>

---
+++ products/vince/aospa_vince.mk
@@ -0,0 +1,48 @@
+# Copyright (C) 2023 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ifeq (aospa_vince,$(TARGET_PRODUCT))
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# All components inherited here go to system_ext image
+$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
+
+# Inherit from vince device
+$(call inherit-product, device/xiaomi/vince/device.mk)
+
+# Inherit from the AOSPA configuration.
+$(call inherit-product, vendor/aospa/target/product/aospa-target.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := vince
+PRODUCT_NAME := aospa_vince
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi 5 Plus
+PRODUCT_MANUFACTURER := Xiaomi
+TARGET_VENDOR := Xiaomi
+BOARD_VENDOR := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Boot animation resolution.
+TARGET_BOOT_ANIMATION_RES := 720
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="vince-user 8.1.0 OPM1.171019.019 V11.0.2.0.OEGMIXM release-keys"
+
+endif
